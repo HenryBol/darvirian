688c912e0e61af753b814a4ee5886fe48118defa
Yaocong111 <42520221+Yaocong111@users.noreply.github.com>
2020-04-06 11:44:05 +0100

    Update File_reader.py

--- File_reader.py
+++ File_reader.py
@@ -6,12 +6,15 @@ Created on Tue March 31 22:39:54 2020
 @author: yaoyao
 """
 import json
+import csv
 import os, stat
+import sys
+import codecs
 from pprint import pprint
 Dataset={}
 Datascore={}
 retval = os.getcwd()
-#print("当前工作目录为  ：" + retval)
+#print("current working folder  ：" + retval)
 #os.chdir ('/Users/yaoyao/Desktop/CORD-19-research-challenge/biorxiv_medrxiv/biorxiv_medrxiv')
 #os.chmod('test1.txt', stat.S_IROTH)
 index=0
@@ -24,6 +27,18 @@ for info in os.listdir(r'/Users/yaoyao/Desktop/CORD-19-research-challenge/biorxi
         Dataset[index]=data
         index+=1
         Datascore[index]=rank(data)# put the search engine result here for each file
+        with open(str(index)+'.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter='\t', quoting=csv.QUOTE_ALL)
+            flag = True
+            for line in f:
+                dic = json.loads(line[0:-1])
+                if flag:
+                    keys = list(dic.keys())
+                    writer.writerow(keys) 
+                    flag = False
+                writer.writerow(list(dic.values()))
+        csvfile.close()
+    f.close()
 result=sorted(Datascore.items(), lambda x, y: cmp(x[1], y[1]))
         
         
